1cfcc9ad3391608b7517431246b58c3dff62ff3e
W-ill-c <williamcollett36@gmail.com>
2022-12-31 12:55:39 +0000

    responsive background

--- src/components/mainBody/mainBody.js
+++ src/components/mainBody/mainBody.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function MainBody(){
     return(    
         <section className='mainBody'>
-            Hello
+            <h1>Hello</h1>
             <img src={require("../../images/backgroundMeal.jpg")} alt="Burger and Fries Background Image" className='backgroundImage'/>
         </section>
     )
