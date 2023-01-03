bc9c503884f2d65823c90c1b752e2ad6410f5c18
W-ill-c <williamcollett36@gmail.com>
2023-01-03 17:53:02 +0000

    input section laptop width all done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import MainBody from './components/mainBody/mainBody'
+import WholeSearchSection from './components/search/search'
 import './components/mainBody/mainBody.scss'
+import './components/search/search.scss'
 
 function App() {
   return (
     <div className="App">
       <MainBody />
+      <WholeSearchSection />
+      <footer className='footer'></footer>
     </div>
   );
 }
--- src/components/mainBody/mainBody.js
+++ src/components/mainBody/mainBody.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function MainBody(){
     return(    
         <section className='mainBody'>
-            <h1>Hello</h1>
             <img src={require("../../images/backgroundMeal.jpg")} alt="Burger and Fries Background Image" className='backgroundImage'/>
         </section>
     )
