960c95836d78c102bc57577548818019b5a2800f
W-ill-c <115991146+W-ill-c@users.noreply.github.com>
2023-01-04 16:00:51 +0000

    Search section (#2)
    * input section laptop width all done
    
    * input for laptop width update and main body scss update
    
    * input section responsiveness all done

---
+++ src/components/search/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function UserInput(){
+    return(    
+        <section className='userInput'>
+          <form>
+            <input placeholder='Enter your postcode'></input>
+            <label><h1>Search</h1></label>
+          </form>
+        </section>
+    )
+}
