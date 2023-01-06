cb871e11190fed87f182029786ef621c62ae0a31
W-ill-c <williamcollett36@gmail.com>
2023-01-06 12:50:48 +0000

    nav at phone size mostly done

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -2,9 +2,10 @@ import React from 'react';
 import NavButtons from './navButtons'
 import NavTitle from './navTitle'
 
+
 export default function NavBar(){
     return(    
-        <header className='navBar'>
+        <header className='navBar navBarActive'>
             <NavTitle />
             <NavButtons />
         </header>
